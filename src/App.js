c768c2d675ea1f387798d76b888ec9887c719526
Zac <azac281@gmail.com>
2021-01-01 21:17:55 -0600

    created the todo list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
         <h1>Todo list</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function TodoList() {
-  return <div></div>;
+  return (
+    <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+  );
 }
