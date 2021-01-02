9f80cdb10e43a7cf79ee8bdfe28c3bf61ff02af3
Zac <azac281@gmail.com>
2021-01-01 22:32:45 -0600

    Added completed toggle functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,12 +5,28 @@ export default function Todo({ text, todo, todos, setTodos }) {
     setTodos(todos.filter((el) => el.id !== todo.id));
   };
 
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <div>
       <div className="todo">
-        <li className="todo-item">{text}</li>
+        <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+          {text}
+        </li>
 
-        <button className="complete-btn">
+        <button className="complete-btn" onClick={completeHandler}>
           <i className="fas fa-check"></i>
         </button>
 
