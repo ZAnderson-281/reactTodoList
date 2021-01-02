3bae21db78b44a659eb3f8eb9fe886a560480d78
Zac <azac281@gmail.com>
2021-01-01 22:23:41 -0600

    added working remove button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function Todo({ text }) {
+export default function Todo({ text, todo, todos, setTodos }) {
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+
   return (
     <div>
       <div className="todo">
@@ -10,7 +14,7 @@ export default function Todo({ text }) {
           <i className="fas fa-check"></i>
         </button>
 
-        <button className="trash-btn">
+        <button className="trash-btn" onClick={deleteHandler}>
           <i className="fas fa-trash"></i>
         </button>
       </div>
